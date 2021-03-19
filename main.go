89c0bd0501d6c6c6f56fd29e07c45cb82d1e1a2e
ckyrouac <ckyrouac@redhat.com>
2021-03-19 12:15:52 -0400

    Fix namespace lookup in openshift

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -55,10 +56,16 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 
-	//clientCfg, _ := clientcmd.NewDefaultClientConfigLoadingRules().Load()
-	//namespace := clientCfg.Contexts[clientCfg.CurrentContext].Namespace
-	namespace := "olm-test"
-	//setupLog.Info("Running in namespace: " + namespace)
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+	namespace, _, err := kubeconfig.Namespace()
+
+	if err != nil {
+		setupLog.Error(err, "error loading namespace")
+	}
+	setupLog.Info("Running in namespace: " + namespace)
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
