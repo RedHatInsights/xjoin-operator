05aaff5e4ec332323e5f4db752c887d9d9c3dd50
ckyrouac <ckyrouac@redhat.com>
2021-03-30 08:54:12 -0400

    Bump version

--- main.go
+++ main.go
@@ -110,6 +110,7 @@ func main() {
 
 	metrics.Init()
 
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
