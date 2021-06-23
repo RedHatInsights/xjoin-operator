7498b4e953e4de9736ef0c455ef2a0f606c5dd7d
Ryan Felton <rfelton@redhat.com>
2021-06-23 15:11:34 -0400

    added the updating script and the job to run it

---
+++ scripts/update_mapping.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { buildMappingsFor } = require("json-schema-to-es-mapping");
+
+const typeMap = {
+    "string": "keyword",
+    "integer": "keyword",
+    "boolean": "boolean",
+    "object": "object"
+}
+
+// function getEsType(key, value) {
+//     console.log(key);
+//     fieldType = value["type"];
+//     indexType = typeMap[fieldType];
+//     if (fieldType == "array") {
+//         console.log("array");
+//         console.log(key);
+//         console.log(value);
+//         value = value["items"];
+//         indexType = getEsType(key, value);
+//     }
+//     if (fieldType == "object") {
+//         //do something else
+//         properties = {};
+//         for (const [key, value] of Object.entries(value["properties"])) {
+            
+//         }
+//         return {
+//             "type": indexType,
+//             "properties": properties
+//         };
+//     }
+
+//     return { "type": indexType };
+// }
+
+try {
+    var myArgs = process.argv.slice(2);
+    schemaPath = myArgs[0];
+
+    console.log(schemaPath)
+
+    if (schemaPath == undefined) {
+        schemaPath = './inventory-schemas/schemas/system_profile/v1.yaml'
+    }
+
+    let schemaFileContent = fs.readFileSync(myArgs[0], 'utf8');
+    let schemaData = yaml.safeLoad(schemaFileContent);
+    // let schema = schemaData["$defs"]["SystemProfile"]["properties"]
+    let schema = schemaData["$defs"]["SystemProfile"]
+
+    let mappingFilePath = '../deploy/operator.yml'
+    let newMappingFilePath = '../deploy/operator_new.yml'
+    let mappingFileContent = fs.readFileSync(mappingFilePath, 'utf8');
+    let mappingData = yaml.safeLoad(mappingFileContent);
+
+
+    
+    console.log("system profile YAML")
+    console.log(schema);
+    
+    console.log("operator YAML")
+    console.log(mappingData["objects"][3]["data"]["elasticsearch.index.template"])
+    console.log(typeof(mappingData["objects"][3]["data"]["elasticsearch.index.template"]))
+    let template = JSON.parse(mappingData["objects"][3]["data"]["elasticsearch.index.template"]);
+    console.log("just the mapping part")
+    console.log(template["mappings"]["properties"]["system_profile_facts"])
+    
+    // generate the mapping properties by analizing each entry in the system profile. 
+    // This should probably be recursive
+    
+    new_mapping = buildMappingsFor("system_profile_facts", schema);
+
+    console.log("new_mapping")
+    console.log(new_mapping["mappings"]["system_profile_facts"])
+
+    template["mappings"]["properties"]["system_profile_facts"]["properties"] = new_mapping["mappings"]["system_profile_facts"]["properties"];
+    mappingData["objects"][3]["data"]["elasticsearch.index.template"] = JSON.stringify(template, null, 4);
+    
+    fs.writeFileSync(newMappingFilePath, " >- \n" + yaml.dump(mappingData));
+
+    //console.log("elements")
+    // for (const [key, value] of Object.entries(schema)) {
+    //     //what to do about arrays? I think they just inherit the type of their member since ES doesn't differentiat 
+    //     if (value["x-indexed"] != false)
+    //         new_mapping[key] = getEsType(key, value) }
+    // }
+} catch (e) {
+    console.log(e);
+}
