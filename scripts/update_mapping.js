c20d55896f6b9e9e42ef37cd4374f8d30c938bcd
Ryan Felton <rfelton@redhat.com>
2021-06-30 16:33:34 -0400

    now recognizing x-indexed

--- scripts/update_mapping.js
+++ scripts/update_mapping.js
@@ -1,6 +1,26 @@
+// When running this script add the path to the schema file to use as argv
+// e.g. node update_mapping.js path/to/file.yml
+
 const fs = require('fs');
 const yaml = require('js-yaml');
 const { buildMappingsFor } = require("json-schema-to-es-mapping");
+
+function remove_blocked_fields(schema) {
+    for (const [key, value] of Object.entries(schema["properties"])) {
+        console.log(key)
+        console.log(value["type"]);
+
+        console.log(typeof(value));
+
+        if ("x-indexed" in value && value["x-indexed"] == false) {
+            console.log("found x-indexed: " + value["x-indexed"]);
+            delete schema["properties"][key]; 
+        }
+    }
+
+    return schema;
+}
+
 
 try {
     var myArgs = process.argv.slice(2);
@@ -22,7 +42,7 @@ try {
 
     let template = JSON.parse(mappingData["objects"][3]["data"]["elasticsearch.index.template"]);
 
-    
+    schema = remove_blocked_fields(schema);
     new_mapping = buildMappingsFor("system_profile_facts", schema);
 
     console.log("new_mapping")
@@ -31,7 +51,7 @@ try {
     template["mappings"]["properties"]["system_profile_facts"]["properties"] = new_mapping["mappings"]["system_profile_facts"]["properties"];
     mappingData["objects"][3]["data"]["elasticsearch.index.template"] = JSON.stringify(template, null, 4);
     
-    fs.writeFileSync(mappingFilePath, yaml.dump(mappingData,{"quotingType": ""} ));
+    fs.writeFileSync(mappingFilePath, yaml.dump(mappingData,{"quotingType": "d"} ));
 
 } catch (e) {
     console.log(e);
