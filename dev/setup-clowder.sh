e0417fce726e8940c482e06b54abd4fc94790a56
ckyrouac <ckyrouac@redhat.com>
2022-02-09 20:34:58 -0500

    Reduce resources deployed via setup-clowder.sh

--- dev/setup-clowder.sh
+++ dev/setup-clowder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+INCLUDE_EXTRA_STUFF=$1
+
 function print_start_message() {
   echo -e "\n********************************************************************************"
   echo -e "* $1"
@@ -7,11 +9,11 @@ function print_start_message() {
 }
 
 function wait_for_pod_to_be_created() {
-  POD_NAME=$1
+  SELECTOR=$1
   # shellcheck disable=SC2034
   for i in {1..120}; do
-    CURIO_OPERATOR_POD=$(kubectl get pods --selector="name=$POD_NAME" -o name)
-    if [ -z "$CURIO_OPERATOR_POD" ]; then
+    POD=$(kubectl get pods --selector="$SELECTOR" -o name)
+    if [ -z "$POD" ]; then
       sleep 1
     else
         break
@@ -35,7 +37,6 @@ sleep 1
 
 # kube_setup.sh
 print_start_message "Running kube-setup.sh"
-KUBE_SETUP_PATH=$1
 CURRENT_DIR=$(pwd)
 mkdir /tmp/kubesetup
 cd /tmp/kubesetup || exit 1
@@ -47,10 +48,8 @@ if [ -z "$KUBE_SETUP_PATH" ]; then
   sed -i 's/^install_cyndi_operator//g' ./kube_setup.sh
   sed -i 's/^install_keda_operator//g' ./kube_setup.sh
   /tmp/kubesetup/kube_setup.sh
-else
-  echo "Using local kube_setup: $KUBE_SETUP_PATH"
-  eval "$KUBE_SETUP_PATH"
 fi
+
 rm -r /tmp/kubesetup
 cd "$CURRENT_DIR" || exit 1
 
@@ -69,18 +68,29 @@ print_start_message "Setting up XJoin operator"
 dev/setup.sh --xjoin-operator --dev --project test
 kubectl apply -f dev/xjoin-generic.configmap.yaml -n test
 
-sleep 5
+sleep 2
 
 # bonfire environment (kafka, connect, etc.)
 print_start_message "Setting up bonfire environment"
-bonfire deploy-env -n test -u cloudservices -f ./dev/clowdenv.yaml
+bonfire process-env -n test -u cloudservices -f ./dev/clowdenv.yaml | oc apply -f - -n test
+wait_for_pod_to_be_created name=kafka-zookeeper-0
+wait_for_pod_to_be_created name=kafka-kafka-0
+kubectl wait --for=condition=ContainersReady=True --selector="strimzi.io/cluster=kafka" pods -n test
 
-sleep 5
+sleep 2
 
 # inventory resources
 print_start_message "Setting up host-inventory"
-bonfire deploy host-inventory -n test
+bonfire process host-inventory -n test --no-get-dependencies | oc apply -f - -n test
+wait_for_pod_to_be_created app=host-inventory,service=db
 kubectl wait --for=condition=ContainersReady=True --selector="app=host-inventory,service=db" pods -n test
+
+# xjoin resources
+bonfire process xjoin -n test --no-get-dependencies | oc apply -f - -n test
+wait_for_pod_to_be_created elasticsearch.k8s.elastic.co/cluster-name=xjoin-elasticsearch
+wait_for_pod_to_be_created pod=xjoin-search-api
+kubectl wait --for=condition=ContainersReady=True --selector="pod=xjoin-search-api" pods -n test
+kubectl wait --for=condition=ContainersReady=True --selector="elasticsearch.k8s.elastic.co/cluster-name=xjoin-elasticsearch" pods -n test
 
 dev/forward-ports-clowder.sh test
 HBI_USER=$(kubectl -n test get secret/host-inventory-db -o custom-columns=:data.username | base64 -d)
@@ -90,28 +100,32 @@ psql -U "$HBI_USER" -h inventory-db -p 5432 -d "$HBI_NAME" -c "ALTER ROLE insigh
 psql -U "$HBI_USER" -h inventory-db -p 5432 -d "$HBI_NAME" -c "CREATE PUBLICATION dbz_publication FOR TABLE hosts;"
 psql -U "$HBI_USER" -h inventory-db -p 5432 -d "$HBI_NAME" -c "CREATE DATABASE test WITH TEMPLATE '$HBI_NAME';"
 
-# cats resources
-print_start_message "Setting up cats"
-kubectl apply -f dev/demo/cats.db.yaml
-kubectl wait --for=condition=ContainersReady=True --selector="app=cats,service=db" pods -n test
-dev/forward-ports-clowder.sh test
-
+# elasticsearch
 print_start_message "Setting up elasticsearch password"
 dev/setup.sh --elasticsearch --project test
 
-# APICurio operator
-print_start_message "Installing Apicurio"
-kubectl create namespace apicurio-registry-operator-namespace
-curl -sSL "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/v1.0.0/docs/resources/install.yaml" | sed "s/apicurio-registry-operator-namespace/test/g" | kubectl apply -f - -n test
-wait_for_pod_to_be_created apicurio-registry-operator
-kubectl wait --for=condition=Ready --selector="name=apicurio-registry-operator" pods -n test
+if [ "$INCLUDE_EXTRA_STUFF" = true ]; then
+  # cats resources
+  print_start_message "Setting up cats"
+  kubectl apply -f dev/demo/cats.db.yaml
+  kubectl wait --for=condition=ContainersReady=True --selector="app=cats,service=db" pods -n test
+  dev/forward-ports-clowder.sh test
 
-# APICurio resource
-kubectl apply -f dev/apicurio.yaml -n test
-wait_for_pod_to_be_created example-apicurioregistry-kafkasql
-kubectl wait --for=condition=Ready --selector="app=example-apicurioregistry-kafkasql" pods -n test
+  # APICurio operator
+  print_start_message "Installing Apicurio"
+  kubectl create namespace apicurio-registry-operator-namespace
+  curl -sSL "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/v1.0.0/docs/resources/install.yaml" | sed "s/apicurio-registry-operator-namespace/test/g" | kubectl apply -f - -n test
+  wait_for_pod_to_be_created name=apicurio-registry-operator
+  kubectl wait --for=condition=Ready --selector="name=apicurio-registry-operator" pods -n test
 
-# XJoin API Gateway
-print_start_message "Setting up xjoin-api-gateway"
-bonfire deploy xjoin-api-gateway -n test
-kubectl wait --for=condition=ContainersReady=True --selector="app=xjoin-api-gateway" pods -n test
+  # APICurio resource
+  kubectl apply -f dev/apicurio.yaml -n test
+  wait_for_pod_to_be_created name=example-apicurioregistry-kafkasql
+  kubectl wait --for=condition=Ready --selector="app=example-apicurioregistry-kafkasql" pods -n test
+
+  # XJoin API Gateway
+  print_start_message "Setting up xjoin-api-gateway"
+  bonfire deploy xjoin-api-gateway -n test --no-get-dependencies
+  kubectl wait --for=condition=ContainersReady=True --selector="app=xjoin-api-gateway" pods -n test
+fi
+
