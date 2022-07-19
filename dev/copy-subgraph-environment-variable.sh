981850ba8d036acb2421c3cd94653f2fba02b21f
ckyrouac <ckyrouac@redhat.com>
2022-07-19 10:41:59 -0400

    Fix copy env var script

--- dev/copy-subgraph-environment-variable.sh
+++ dev/copy-subgraph-environment-variable.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl get -o yaml pods --selector="xjoin.index=$1" | yq eval ".items[0].spec.containers[].env | .[] | select(.name == \"$2\") | .value" -
+kubectl get -o yaml deployments --selector="xjoin.index=$1" | yq eval ".items[0].spec.template.spec.containers[].env | .[] | select(.name == \"$2\") | .value" -
