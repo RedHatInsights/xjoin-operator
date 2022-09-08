bf3841d115c8fb50be62e56e7cf7f04cc5ee0173
ckyrouac <ckyrouac@redhat.com>
2022-09-08 12:20:58 -0400

    Reset index

--- build_catalog.sh
+++ build_catalog.sh
@@ -72,12 +72,12 @@ fi
 ###############################
 #Uncomment to reset the catalog
 ###############################
-#log "Resetting index"
-#./opm index rm -f $CATALOG_IMAGE:latest -c docker --tag $CATALOG_IMAGE:latest -o xjoin-operator
-#docker push $CATALOG_IMAGE:latest
-#export SKIP_VERSION=$version
-#prev_version=""
-#unset REPLACE_VERSION
+log "Resetting index"
+./opm index rm -f $CATALOG_IMAGE:latest -c docker --tag $CATALOG_IMAGE:latest -o xjoin-operator
+docker push $CATALOG_IMAGE:latest
+export SKIP_VERSION=$version
+prev_version=""
+unset REPLACE_VERSION
 
 # Build/push the new bundle
 log "Creating bundle $BUNDLE_IMAGE:$current_commit"
