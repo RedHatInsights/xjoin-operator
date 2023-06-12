07793aed77d70eb4191089a8a63b2f1f2576b984
Chris Kyrouac <ckyrouac@redhat.com>
2023-06-12 09:55:08 -0400

    Add groups column to v1 test data

--- controllers/test/data/hosts/hbi/simple.sql
+++ controllers/test/data/hosts/hbi/simple.sql
@@ -10,7 +10,8 @@ INSERT INTO hosts (
     canonical_facts,
     system_profile_facts,
     stale_timestamp,
-    reporter)
+    reporter,
+    groups)
 VALUES (
     '{{.ID}}',
     '5',
@@ -23,4 +24,5 @@ VALUES (
     '{"test3": "test3a", "test4": ["asdf", "jkl;"], "test5": {"a": {"1": "123"}}, "test6": [{"1": "23"}, {"2": {"3": "345"}}]}',
     '{"arch": "x86-64", "cpu_flags": ["flag1", "flag2"], "yum_repos": [{"name": "repo1", "enabled": true, "base_ur    l": "http://rpms.redhat.com", "gpgcheck": true}], "is_marketplace": false, "number_of_cpus": 1,  "number_of_sockets": 2, "anobject": {"key1": "value1", "key2": "value2", "array1": ["value1", "value2"]}}',
     '2017-01-01 00:00:00',
-    'me')
+    'me',
+    '[]')
