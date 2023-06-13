36261948dcb2e007e2d6074e189cba97838937a3
thearifismail <40067075+thearifismail@users.noreply.github.com>
2023-06-13 10:37:41 -0400

    ESSNTL-4029: Add check deviation for Elasticsearch connector (#83)
    * Add check deviation for Elasticsearch connector
    
    * Update ES connectors check pointed out by Chris' code review
    
    ---------
    
    Co-authored-by: Chris Kyrouac <fijshion@users.noreply.github.com>

--- controllers/components/elasticsearch_connector.go
+++ controllers/components/elasticsearch_connector.go
@@ -1,9 +1,13 @@
 package components
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/go-errors/errors"
 	"github.com/redhatinsights/xjoin-operator/controllers/kafka"
-	"strings"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type ElasticsearchConnector struct {
@@ -48,6 +52,44 @@ func (es *ElasticsearchConnector) Delete() (err error) {
 }
 
 func (es *ElasticsearchConnector) CheckDeviation() (problem, err error) {
+	found, err := es.Exists()
+	if err != nil {
+		return nil, errors.Wrap(err, 0)
+	}
+	if !found {
+		return fmt.Errorf("ElasticsearchConnector named, %s, does not exist.", es.Name()), nil
+	}
+
+	esConPtr, err := es.KafkaClient.GetConnector(es.Name())
+
+	if err != nil {
+		return nil, fmt.Errorf("Error encountered when getting ElasticsearchConnector: %w", err)
+	}
+
+	if esConPtr == nil {
+		return fmt.Errorf("Problem encountered when getting ElasticsearchConnector: %w", err), nil
+	} else {
+		var allConns *unstructured.UnstructuredList
+		allConns, err := es.KafkaClient.ListConnectors()
+
+		if err != nil {
+			return nil, fmt.Errorf("Error encountered when listing connectors: %w", err)
+		}
+
+		if allConns.Items == nil || len(allConns.Items) == 0 {
+			return fmt.Errorf("No Elasticsearch connector available"), nil
+		}
+
+		for _, conn := range allConns.Items {
+			if esConPtr.GetName() == conn.GetName() {
+				if equality.Semantic.DeepEqual(*esConPtr, conn) {
+					return nil, nil
+				} else {
+					return fmt.Errorf("Elasticsearch connector named %s has changed", conn.GetName()), nil
+				}
+			}
+		}
+	}
 	return
 }
 
