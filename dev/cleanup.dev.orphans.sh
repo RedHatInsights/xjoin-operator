4a5aed0e0399292c50ba54b874278e93f2dacac0
Chris Kyrouac <ckyrouac@redhat.com>
2023-07-06 14:30:41 -0400

    Namespace param for cleanup.dev.orphans script

--- dev/cleanup.dev.orphans.sh
+++ dev/cleanup.dev.orphans.sh
@@ -1,52 +1,61 @@
 #!/bin/bash
-kubectl get xjoindatasourcepipeline -o custom-columns=name:metadata.name --no-headers | while read -r datasourcepipeline ; do
-  kubectl patch xjoindatasourcepipeline "$datasourcepipeline" -p '{"metadata":{"finalizers":null}}' --type=merge
-  kubectl delete xjoindatasourcepipeline "$datasourcepipeline"
+
+PROJECT_NAME=$1
+
+if [ -z "$PROJECT_NAME" ]; then
+  PROJECT_NAME="test"
+fi
+
+echo "Using namespace $PROJECT_NAME"
+
+kubectl get xjoindatasourcepipeline -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | while read -r datasourcepipeline ; do
+  kubectl patch xjoindatasourcepipeline "$datasourcepipeline" -p '{"metadata":{"finalizers":null}}' --type=merge -n "$PROJECT_NAME"
+  kubectl delete xjoindatasourcepipeline "$datasourcepipeline" -n "$PROJECT_NAME"
 done
 
-kubectl get xjoindatasource -o custom-columns=name:metadata.name --no-headers | while read -r datasource ; do
-  kubectl patch xjoindatasource "$datasource" -p '{"metadata":{"finalizers":null}}' --type=merge
-  kubectl delete xjoindatasource "$datasource"
+kubectl get xjoindatasource -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | while read -r datasource ; do
+  kubectl patch xjoindatasource "$datasource" -p '{"metadata":{"finalizers":null}}' --type=merge -n "$PROJECT_NAME"
+  kubectl delete xjoindatasource "$datasource" -n "$PROJECT_NAME"
 done
 
-kubectl get xjoinindex -o custom-columns=name:metadata.name --no-headers | while read -r index ; do
-  kubectl patch xjoinindex "$index" -p '{"metadata":{"finalizers":null}}' --type=merge
-  kubectl delete xjoinindex "$index"
+kubectl get xjoinindex -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | while read -r index ; do
+  kubectl patch xjoinindex "$index" -p '{"metadata":{"finalizers":null}}' --type=merge -n "$PROJECT_NAME"
+  kubectl delete xjoinindex "$index" -n "$PROJECT_NAME"
 done
 
-kubectl get xjoinindexpipeline -o custom-columns=name:metadata.name --no-headers | while read -r indexpipeline ; do
-  kubectl patch xjoinindexpipeline "$indexpipeline" -p '{"metadata":{"finalizers":null}}' --type=merge
-  kubectl delete xjoinindexpipeline "$indexpipeline"
+kubectl get xjoinindexpipeline -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | while read -r indexpipeline ; do
+  kubectl patch xjoinindexpipeline "$indexpipeline" -p '{"metadata":{"finalizers":null}}' --type=merge -n "$PROJECT_NAME"
+  kubectl delete xjoinindexpipeline "$indexpipeline" -n "$PROJECT_NAME"
 done
 
-kubectl get xjoinindexvalidator -o custom-columns=name:metadata.name --no-headers | while read -r indexvalidator ; do
-  kubectl patch xjoinindexvalidator "$indexvalidator" -p '{"metadata":{"finalizers":null}}' --type=merge
-  kubectl delete xjoinindexvalidator "$indexvalidator"
+kubectl get xjoinindexvalidator -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | while read -r indexvalidator ; do
+  kubectl patch xjoinindexvalidator "$indexvalidator" -p '{"metadata":{"finalizers":null}}' --type=merge -n "$PROJECT_NAME"
+  kubectl delete xjoinindexvalidator "$indexvalidator" -n "$PROJECT_NAME"
 done
 
-kubectl get deployments -n test -o custom-columns=name:metadata.name --no-headers | grep xjoin-core | while read -r xjoincore ; do
-  kubectl delete deployment "$xjoincore"
+kubectl get deployments -o custom-columns=name:metadata.name --no-headers -n "$PROJECT_NAME" | grep xjoin-core | while read -r xjoincore ; do
+  kubectl delete deployment "$xjoincore" -n "$PROJECT_NAME"
 done
 
 echo "Deleting connectors.."
-kubectl -n test get KafkaConnector -o custom-columns=name:metadata.name | grep indexpipeline | while read -r connector ; do
-    kubectl delete KafkaConnector "$connector" -n test
+kubectl get KafkaConnector -o custom-columns=name:metadata.name -n "$PROJECT_NAME" | grep indexpipeline | while read -r connector ; do
+    kubectl delete KafkaConnector "$connector" -n "$PROJECT_NAME"
 done
-kubectl -n test get KafkaConnector -o custom-columns=name:metadata.name | grep datasourcepipeline | while read -r connector ; do
-    kubectl delete KafkaConnector "$connector" -n test
+kubectl get KafkaConnector -o custom-columns=name:metadata.name -n "$PROJECT_NAME" | grep datasourcepipeline | while read -r connector ; do
+    kubectl delete KafkaConnector "$connector" -n "$PROJECT_NAME"
 done
 
 echo "Deleting topics.."
-kubectl -n test get KafkaTopic -o custom-columns=name:metadata.name | grep indexpipeline | while read -r topic ; do
-    kubectl delete KafkaTopic "$topic" -n test
+kubectl get KafkaTopic -o custom-columns=name:metadata.name -n "$PROJECT_NAME" | grep indexpipeline | while read -r topic ; do
+    kubectl delete KafkaTopic "$topic" -n "$PROJECT_NAME"
 done
-kubectl -n test get KafkaTopic -o custom-columns=name:metadata.name | grep datasourcepipeline | while read -r topic ; do
-    kubectl delete KafkaTopic "$topic" -n test
+kubectl get KafkaTopic -o custom-columns=name:metadata.name -n "$PROJECT_NAME" | grep datasourcepipeline | while read -r topic ; do
+    kubectl delete KafkaTopic "$topic" -n "$PROJECT_NAME"
 done
 
 echo "Deleting avro subjects.."
-APICURIO_HOSTNAME=apicurio.test.svc
-APICURIO_PORT=10001
+APICURIO_HOSTNAME="xjoin-apicurio-service.$PROJECT_NAME.svc"
+APICURIO_PORT=10000
 artifacts=$(curl "http://$APICURIO_HOSTNAME:$APICURIO_PORT/apis/registry/v2/search/artifacts?limit=100" | jq '.artifacts|map(.id)|@sh')
 artifacts=($artifacts)
 total=${#artifacts[@]}
@@ -73,16 +82,16 @@ for i in "${!artifacts[@]}"; do
 done
 
 echo "Deleting replication slots"
-HBI_USER=$(kubectl get secret/host-inventory-db -o custom-columns=:data.username | base64 -d)
-HBI_NAME=$(kubectl get secret/host-inventory-db -o custom-columns=:data.name | base64 -d)
-HBI_HOSTNAME=host-inventory-db.test.svc
+HBI_USER=$(kubectl get secret/host-inventory-db -o custom-columns=:data.username -n "$PROJECT_NAME" | base64 -d)
+HBI_NAME=$(kubectl get secret/host-inventory-db -o custom-columns=:data.name -n "$PROJECT_NAME" | base64 -d)
+HBI_HOSTNAME="host-inventory-db.$PROJECT_NAME.svc"
 psql -U "$HBI_USER" -h "$HBI_HOSTNAME" -p 5432 -d "$HBI_NAME" -t -c "SELECT slot_name from pg_catalog.pg_replication_slots" | while read -r slot ; do
   psql -U "$HBI_USER" -h "$HBI_HOSTNAME" -p 5432 -d "$HBI_NAME" -c "SELECT pg_drop_replication_slot('$slot');"
 done
 
 echo "Deleting ES indexes"
-ES_PASSWORD=$(kubectl get secret/xjoin-elasticsearch-es-elastic-user -o custom-columns=:data.elastic | base64 -d)
-ES_HOSTNAME=xjoin-elasticsearch-es-default.test.svc
+ES_PASSWORD=$(kubectl get secret/xjoin-elasticsearch-es-elastic-user -o custom-columns=:data.elastic -n "$PROJECT_NAME"| base64 -d)
+ES_HOSTNAME="xjoin-elasticsearch-es-default.$PROJECT_NAME.svc"
 curl -u "elastic:$ES_PASSWORD" "http://$ES_HOSTNAME:9200/_cat/indices?format=json" | jq '.[] | .index' | grep -e xjoinindexpipeline -e test.hosts -e xjointest -e prefix | while read -r index ; do
   index="${index:1}"
   index="${index::-1}"
@@ -90,8 +99,8 @@ curl -u "elastic:$ES_PASSWORD" "http://$ES_HOSTNAME:9200/_cat/indices?format=jso
 done
 
 echo "Deleting ES pipelines"
-ES_PASSWORD=$(kubectl get secret/xjoin-elasticsearch-es-elastic-user -o custom-columns=:data.elastic | base64 -d)
-ES_HOSTNAME=xjoin-elasticsearch-es-default.test.svc
+ES_PASSWORD=$(kubectl get secret/xjoin-elasticsearch-es-elastic-user -o custom-columns=:data.elastic -n "$PROJECT_NAME"| base64 -d)
+ES_HOSTNAME="xjoin-elasticsearch-es-default.$PROJECT_NAME.svc"
 curl -u "elastic:$ES_PASSWORD" "http://$ES_HOSTNAME:9200/_ingest/pipeline?format=json" | jq 'keys[]' | grep -e xjoinindexpipeline -e prefix -e test.hosts -e xjointest | while read -r index ; do
   index="${index:1}"
   index="${index::-1}"
@@ -99,14 +108,14 @@ curl -u "elastic:$ES_PASSWORD" "http://$ES_HOSTNAME:9200/_ingest/pipeline?format
 done
 
 echo "Deleting subgraph pods"
-kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-hosts'
-kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-cats'
-kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-cats'
-kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-hosts-hbi-tags'
+kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-hosts' -n "$PROJECT_NAME"
+kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-cats' -n "$PROJECT_NAME"
+kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-cats' -n "$PROJECT_NAME"
+kubectl delete deployments --selector='xjoin.index=xjoinindexpipeline-hosts-hbi-tags' -n "$PROJECT_NAME"
 
-kubectl delete services --selector='xjoin.index=xjoinindexpipeline-hosts'
-kubectl delete services --selector='xjoin.index=xjoinindexpipeline-cats'
-kubectl delete services --selector='xjoin.index=xjoinindexpipeline-cats'
-kubectl delete services --selector='xjoin.index=xjoinindexpipeline-hosts-hbi-tags'
+kubectl delete services --selector='xjoin.index=xjoinindexpipeline-hosts' -n "$PROJECT_NAME"
+kubectl delete services --selector='xjoin.index=xjoinindexpipeline-cats' -n "$PROJECT_NAME"
+kubectl delete services --selector='xjoin.index=xjoinindexpipeline-cats' -n "$PROJECT_NAME"
+kubectl delete services --selector='xjoin.index=xjoinindexpipeline-hosts-hbi-tags' -n "$PROJECT_NAME"
 
-kubectl delete pods --selector='xjoin.component.name=XJoinIndexValidator'
+kubectl delete pods --selector='xjoin.component.name=XJoinIndexValidator' -n "$PROJECT_NAME"
