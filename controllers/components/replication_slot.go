95cf4b82553cc2054414981632aefccfe204e3f6
Chris Kyrouac <ckyrouac@redhat.com>
2023-07-26 14:27:57 -0400

    Fix bug in replication slot list installed versions

--- controllers/components/replication_slot.go
+++ controllers/components/replication_slot.go
@@ -72,7 +72,11 @@ func (dc *ReplicationSlot) ListInstalledVersions() (versions []string, err error
 	}
 
 	for _, slot := range slots {
-		versions = append(versions, strings.Split(slot, "_")[len(slots)])
+		if strings.Index(slot, dc.name) == 0 {
+			version := strings.Split(slot, dc.name)[1]
+			versionPieces := strings.Split(version, "_")
+			versions = append(versions, versionPieces[len(versionPieces)])
+		}
 	}
 	return
 }
