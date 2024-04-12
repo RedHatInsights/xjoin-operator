c00aee20834077dd57a895764cb9b393f10b1e33
Asa Price <aprice@redhat.com>
2024-04-12 12:33:17 -0400

    Test: Revert temp docker dir

--- build_catalog.sh
+++ build_catalog.sh
@@ -48,18 +48,7 @@ function build_a_tag ()
   export PATH=${GOUNPACK}/go/bin:$PATH
 
   # Login to docker
-  # Create tmp dir to store data in during job run (do NOT store in $WORKSPACE)
-  export TMP_JOB_DIR=$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")
-  echo "job tmp dir location: $TMP_JOB_DIR"
-
-  function job_cleanup() {
-      echo "cleaning up job tmp dir: $TMP_JOB_DIR"
-      rm -fr $TMP_JOB_DIR
-  }
-
-  trap job_cleanup EXIT ERR SIGINT SIGTERM
-
-  DOCKER_CONF="$TMP_JOB_DIR/.docker"
+  export DOCKER_CONFIG="$PWD/.docker"
   mkdir -p "$DOCKER_CONFIG"
 
   docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
